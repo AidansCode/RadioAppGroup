802bc2a7a4d1f5b816faef39cdfe5096b93e53c6
Zee <amurphey7@gmail.com>
2019-09-12 11:13:39 -0400

    Initial commit

---
+++ src/com/cheekcollectors/radioapp/RadioApp.java
@@ -0,0 +1,16 @@
+package com.cheekcollectors.radioapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RadioApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
