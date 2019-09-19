dbf435f98fa6972aa4c4d9ea6479aad58e928975
AngelAvelarBonilla <angelavelarbonilla@gmail.com>
2019-09-19 19:48:55 -0400

    GUI is visible now, however, am/fm button functionality and the image for the power button still need to be added.

--- src/com/cheekcollectors/radioapp/RadioAppGUI.java
+++ src/com/cheekcollectors/radioapp/RadioAppGUI.java
@@ -1,14 +1,19 @@
 package com.cheekcollectors.radioapp;
 
+import com.cheekcollectors.radioapp.eventhandlers.PowerButtonEvent;
+import com.cheekcollectors.radioapp.eventhandlers.SeekButtonEvent;
 import com.cheekcollectors.radioapp.eventhandlers.SetSelectFavoriteEvent;
 import com.cheekcollectors.radioapp.fileio.RadioDataManager;
 import com.cheekcollectors.radioapp.nodes.FavoriteButton;
 import com.cheekcollectors.radioapp.radio.Radio;
+import com.cheekcollectors.radioapp.radio.SeekDirection;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -87,6 +92,37 @@ public class RadioAppGUI extends Application {
             favoriteButtons[i] = new FavoriteButton(Integer.toString(i + 1), i);
             favoriteButtons[i].setOnMouseClicked(new SetSelectFavoriteEvent());
         }
+
+        //TODO amFmButton.setOnAction(new FrequencyTypeEvent());
+
+        seekBackButton.setOnAction(new SeekButtonEvent(SeekDirection.DOWN));
+        seekForwardButton.setOnAction(new SeekButtonEvent(SeekDirection.UP));
+
+        powerButton.setPrefSize(30, 30);
+        //TODO powerButton.setStyle("-fx-background-image: url('resources/power.png'); -fx-background-size: 20px; -fx-background-repeat: no-repeat; -fx-background-position: center;");
+        powerButton.setOnAction(new PowerButtonEvent());
+
+        statusPane = new TextField(radio.getStatusString());
+        statusPane.setAlignment(Pos.CENTER);
+        statusPane.setDisable(true);
+        statusPane.setStyle("-fx-background-color: darkolivegreen; -fx-opacity: 1; -fx-text-fill: white");
+
+        frequencyTypeMenu.getChildren().addAll(amFmButton);
+        topMenu.getChildren().addAll(powerButton, frequencyTypeMenu);
+        middleMenu.getChildren().addAll(seekBackButton, statusPane, seekForwardButton);
+        bottomMenu.getChildren().addAll(favoriteButtons);
+
+        BorderPane mainLayout = new BorderPane();
+        mainLayout.setTop(topMenu);
+        mainLayout.setCenter(middleMenu);
+        mainLayout.setBottom(bottomMenu);
+
+        Scene scene = new Scene(mainLayout, 500, 220);
+        primaryStage.setTitle(APP_TITLE);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+
+        mainLayout.requestFocus();
 
     }
 
