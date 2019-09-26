c44d5afaf2eed160c26ea854b08947f50eb54409
AngelAvelarBonilla <angelavelarbonilla@gmail.com>
2019-09-26 19:49:19 -0400

    Merge branch 'master' of D:\Avelar-Bonilla\CSC 223\RadioADT Project\RadioAppGroup with conflicts.

--- src/com/cheekcollectors/radioapp/RadioAppGUI.java
+++ src/com/cheekcollectors/radioapp/RadioAppGUI.java
@@ -1,6 +1,5 @@
 package com.cheekcollectors.radioapp;
 
-import com.cheekcollectors.radioapp.eventhandlers.FrequencyTypeEvent;
 import com.cheekcollectors.radioapp.eventhandlers.PowerButtonEvent;
 import com.cheekcollectors.radioapp.eventhandlers.SeekButtonEvent;
 import com.cheekcollectors.radioapp.eventhandlers.SetSelectFavoriteEvent;
@@ -30,43 +29,22 @@ public class RadioAppGUI extends Application {
         launch(args);
     }
 
-    /**
-     * Returns the app's instance of the Radio class
-     * @return Radio The instance of the Radio being used
-     */
     public static Radio getRadio() {
         return radio;
     }
 
-    /**
-     * Updates the status box of the radio with a given string
-     * @param status The string to set the status to
-     */
     public static void updateStatus(String status) {
         statusPane.setText(status);
     }
 
-    /**
-     * Return the radio app's current FavoriteMode
-     * @return FavoriteMode The app's current FavoriteMode
-     */
     public static FavoriteMode getFavoriteMode() {
         return favoriteMode;
     }
 
-    /**
-     * Set the app's current favorite mode
-     * @param favoriteMode The new favorite mode of the app
-     */
     public static void setFavoriteMode(FavoriteMode favoriteMode) {
         RadioAppGUI.favoriteMode = favoriteMode;
     }
 
-    /**
-     * Initialize the JavaFX application
-     * @param primaryStage The primary Stage of the application
-     * @throws Exception
-     */
     public void start(Stage primaryStage) throws Exception {
         radio = RadioDataManager.readData();
         if (radio == null) {
@@ -78,20 +56,12 @@ public class RadioAppGUI extends Application {
         primaryStage.show();
     }
 
-    /**
-     * Called when the JavaFX application is closed (program ending)
-     * @throws Exception
-     */
     @Override
     public void stop() throws Exception {
         super.stop();
         RadioDataManager.saveData(radio);
     }
 
-    /**
-     * Initialize the layout of the application
-     * @param primaryStage The primary Stage of the application
-     */
     private void initLayout(Stage primaryStage) {
         VBox topMenu = new VBox();
         topMenu.setSpacing(25);
@@ -123,13 +93,13 @@ public class RadioAppGUI extends Application {
             favoriteButtons[i].setOnMouseClicked(new SetSelectFavoriteEvent());
         }
 
-        amFmButton.setOnAction(new FrequencyTypeEvent());
+        //TODO amFmButton.setOnAction(new FrequencyTypeEvent());
 
         seekBackButton.setOnAction(new SeekButtonEvent(SeekDirection.DOWN));
         seekForwardButton.setOnAction(new SeekButtonEvent(SeekDirection.UP));
 
         powerButton.setPrefSize(30, 30);
-        powerButton.setStyle("-fx-background-image: url('resources/power.png'); -fx-background-size: 20px; -fx-background-repeat: no-repeat; -fx-background-position: center;");
+        //TODO powerButton.setStyle("-fx-background-image: url('resources/power.png'); -fx-background-size: 20px; -fx-background-repeat: no-repeat; -fx-background-position: center;");
         powerButton.setOnAction(new PowerButtonEvent());
 
         statusPane = new TextField(radio.getStatusString());
--- src/com/cheekcollectors/radioapp/eventhandlers/FrequencyTypeEvent.java
+++ src/com/cheekcollectors/radioapp/eventhandlers/FrequencyTypeEvent.java
@@ -1,20 +1,22 @@
 package com.cheekcollectors.radioapp.eventhandlers;
 
 import com.cheekcollectors.radioapp.RadioAppGUI;
+import com.cheekcollectors.radioapp.radio.FrequencyType;
 import com.cheekcollectors.radioapp.radio.Radio;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
-public class FrequencyTypeEvent implements EventHandler<ActionEvent> {
+public class FrequencyTypeEvent {
 
-    /**
-     * Receives the event for the FrequencyTypeEvent (when the frequency type is changed)
-     * @param actionEvent The ActionEvent for the FrequencyTypeEvent
-     */
+    private FrequencyType frequencyType;
+
+    public FrequencyTypeEvent(FrequencyType frequencyType) {
+        this.frequencyType = frequencyType;
+    }
+
     public void handle(ActionEvent actionEvent) {
         Radio radio = RadioAppGUI.getRadio();
 
-        radio.toggleFrequencyType();
+        radio.setFrequencyType(frequencyType);
         RadioAppGUI.updateStatus(radio.getStatusString());
     }
 
